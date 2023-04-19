3e1757536f7517dbe6934585cabf0c091c4d0881
Kuehlski <131247217+Kuehlski@users.noreply.github.com>
2023-04-19 17:23:49 +0200

    Create Ex2_2.py

---
+++ Assigments 2/Ex2_2.py
@@ -0,0 +1,17 @@
+import sys
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+keyphrase = sys.argv[3]
+keyphrase = keyphrase.encode()
+
+f1 = open(input_file, "rb")
+f2 = open(output_file, "wb")
+
+
+data = f1.read()
+key_len = len(keyphrase)
+
+for i in range(len(data)):
+    key_byte = keyphrase[i%key_len]
+    f2.write(bytes([data[i] ^ key_byte]))
