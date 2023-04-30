0f72318d975eca94dc8cab3a91a4537fbb35a790
Henri Heyden <henri@heyden5.de>
2023-04-30 23:38:44 +0200

    Added the second task of the second assigment and rewrote client.py

--- Assigments 3/Practical/client.py
+++ Assigments 3/Practical/client.py
@@ -2,23 +2,37 @@ from socket import *
 import sys
 
 
+serverAddress = ""
+serverPort = 0
+
 try:
     serverAddress = sys.argv[1]
     serverPort = int(sys.argv[2])
+
+
 except:
-    print("No valid input given")
-    serverAddress = "localhost"
-    serverPort = 12345
+    try:
+        if sys.argv[1] == "--help":
+            print("usage: python client.py [--help] \nor python client.py (serverAddress) (serverPort)")
+            exit(0)
+    except:
+        pass
+    print("No valid input given, please specify serverAddress and serverPort")
+    exit(-1)
 
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
 
-while True:
-    try:
-        clientSocket.sendto(input().encode(), (serverAddress, serverPort))
-        data = clientSocket.recvfrom(2048)[0]
-        print(data.decode().removesuffix("\n"))
-    except:
-        break
+try:
+    while True:
+        try:
+            clientSocket.sendto(input().encode(), (serverAddress, serverPort))
+            data = clientSocket.recvfrom(2048)[0]
+            print(data.decode().removesuffix("\n"))
+        except EOFError:
+            break
+except KeyboardInterrupt:
+    clientSocket.close()
+    print("exiting...")
 
 
